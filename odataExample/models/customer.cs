79ce82f70be540916f1feb5987566e67c7fc78e6
ian simpson <iansimpson55@hotmail.com>
2020-06-01 12:54:41 +0100

    init

---
+++ odataExample/models/customer.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace odataExample.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
